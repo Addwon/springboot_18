8fc7fe5093bc9081566612e6d8e1a68ff7533938
addwon <addwon@gmail.com>
2018-02-12 10:34:33 -0500

    Initial commit

---
+++ src/main/java/com/example/springboot_18/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.springboot_18;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController{
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
